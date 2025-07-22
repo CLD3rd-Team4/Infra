69eb072be266f2ded227510938251eb6fb79c592
kaiju782 <didwjdah7@sunmoon.ac.kr>
2025-07-22 17:32:05 +0900

    fix: providers 주석처리로 보존

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,6 +4,16 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.4.0"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0.2"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.38.0"
+    }
   }
   required_version = "~> 1.12.0"
 }
@@ -11,3 +21,25 @@ terraform {
 provider "aws" {
   region = "ap-northeast-2"
 }
+
+# data "aws_eks_cluster" "cluster" {
+#   name = module.eks.cluster_name
+# }
+
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = module.eks.cluster_name
+# }
+
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+# }
+
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.cluster.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.cluster.token
+#   }
+# }
