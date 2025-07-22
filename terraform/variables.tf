50726bb279bb871947ad7ddd119bd8904538f7e2
kaiju782 <didwjdah7@sunmoon.ac.kr>
2025-07-22 17:22:38 +0900

    fix: 서비스네임, 프리픽스 누락 되돌림

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -63,3 +63,15 @@ variable "db_password" {
   type        = string
   sensitive   = true
 }
+
+variable "service_name" {
+  description = "The name of the service to be created in EKS"
+  type        = string
+  default     = "mapzip"
+}
+
+variable "service_domain" {
+  description = "The domain name for the service"
+  type        = string
+  default     = "mapzip.shop"
+}
