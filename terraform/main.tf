7dc9b9534883854f2504313c918d8264235c9714
조성욱 <>
2025-07-23 10:01:50 +0900

    feat: add private subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,3 +29,52 @@ module "natgw" {
   common_prefix = local.common_prefix
   common_tags   = local.common_tags
 }
+
+module "subnet_private" {
+  source                = "./modules/network/subnet"
+  vpc_id                = module.vpc.vpc_id
+  cidr_block            = "10.0.2.0/24"
+  availability_zone     = "ap-northeast-2a"
+  map_public_ip_on_launch = false
+  name                  = "private-1a"
+  common_prefix         = local.common_prefix
+  common_tags           = local.common_tags
+}
+
+# 퍼블릭 라우팅 테이블
+resource "aws_route_table" "public" {
+  vpc_id = module.vpc.vpc_id
+  tags = merge(local.common_tags, { Name = "${local.common_prefix}public-rtb" })
+}
+
+# IGW로 0.0.0.0/0 라우트
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = module.igw.igw_id
+}
+
+# 퍼블릭 서브넷 연결
+resource "aws_route_table_association" "public" {
+  subnet_id      = module.subnet_public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# 프라이빗 라우팅 테이블
+resource "aws_route_table" "private" {
+  vpc_id = module.vpc.vpc_id
+  tags = merge(local.common_tags, { Name = "${local.common_prefix}private-rtb" })
+}
+
+# NAT GW로 0.0.0.0/0 라우트
+resource "aws_route" "private_natgw_access" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = module.natgw.natgw_id
+}
+
+# 프라이빗 서브넷 연결
+resource "aws_route_table_association" "private" {
+  subnet_id      = module.subnet_private.id
+  route_table_id = aws_route_table.private.id
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,31 @@
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
+output "subnet_private_id" {
+  value = module.subnet_private.id
+}
+
+output "public_route_table_id" {
+  value = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  value = aws_route_table.private.id
+}
+
+output "public_route_table_association_id" {
+  value = aws_route_table_association.public.id
+}
+
+output "private_route_table_association_id" {
+  value = aws_route_table_association.private.id
+}
+
+output "public_route_id" {
+  value = aws_route.public_internet_access.id
+}
+
+output "private_route_id" {
+  value = aws_route.private_natgw_access.id
+}
