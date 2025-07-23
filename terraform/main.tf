49e75fded2291b45e703bb256a878c140d625d19
조성욱 <>
2025-07-23 10:23:19 +0900

    Public & Private Subnet 개수 수정

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,22 @@ module "vpc" {
   common_tags   = local.common_tags
 }
 
-module "subnet_public" {
+module "igw" {
+  source        = "./modules/network/igw"
+  vpc_id        = module.vpc.vpc_id
+  common_prefix = local.common_prefix
+  common_tags   = local.common_tags
+}
+
+module "natgw" {
+  source        = "./modules/network/natgw"
+  subnet_id     = module.subnet_public_1a.id
+  common_prefix = local.common_prefix
+  common_tags   = local.common_tags
+}
+
+# 퍼블릭 서브넷 2개
+module "subnet_public_1a" {
   source                = "./modules/network/subnet"
   vpc_id                = module.vpc.vpc_id
   cidr_block            = "10.0.1.0/24"
@@ -16,27 +31,55 @@ module "subnet_public" {
   common_tags           = local.common_tags
 }
 
-module "igw" {
-  source        = "./modules/network/igw"
-  vpc_id        = module.vpc.vpc_id
-  common_prefix = local.common_prefix
-  common_tags   = local.common_tags
-}
-
-module "natgw" {
-  source        = "./modules/network/natgw"
-  subnet_id     = module.subnet_public.id
-  common_prefix = local.common_prefix
-  common_tags   = local.common_tags
-}
-
-module "subnet_private" {
+module "subnet_public_2c" {
   source                = "./modules/network/subnet"
   vpc_id                = module.vpc.vpc_id
   cidr_block            = "10.0.2.0/24"
+  availability_zone     = "ap-northeast-2c"
+  map_public_ip_on_launch = true
+  name                  = "public-2c"
+  common_prefix         = local.common_prefix
+  common_tags           = local.common_tags
+}
+
+# 프라이빗 서브넷 4개
+module "subnet_private_1a_1" {
+  source                = "./modules/network/subnet"
+  vpc_id                = module.vpc.vpc_id
+  cidr_block            = "10.0.10.0/24"
   availability_zone     = "ap-northeast-2a"
   map_public_ip_on_launch = false
-  name                  = "private-1a"
+  name                  = "private-1a-1"
+  common_prefix         = local.common_prefix
+  common_tags           = local.common_tags
+}
+module "subnet_private_1a_2" {
+  source                = "./modules/network/subnet"
+  vpc_id                = module.vpc.vpc_id
+  cidr_block            = "10.0.11.0/24"
+  availability_zone     = "ap-northeast-2a"
+  map_public_ip_on_launch = false
+  name                  = "private-1a-2"
+  common_prefix         = local.common_prefix
+  common_tags           = local.common_tags
+}
+module "subnet_private_2c_1" {
+  source                = "./modules/network/subnet"
+  vpc_id                = module.vpc.vpc_id
+  cidr_block            = "10.0.12.0/24"
+  availability_zone     = "ap-northeast-2c"
+  map_public_ip_on_launch = false
+  name                  = "private-2c-1"
+  common_prefix         = local.common_prefix
+  common_tags           = local.common_tags
+}
+module "subnet_private_2c_2" {
+  source                = "./modules/network/subnet"
+  vpc_id                = module.vpc.vpc_id
+  cidr_block            = "10.0.13.0/24"
+  availability_zone     = "ap-northeast-2c"
+  map_public_ip_on_launch = false
+  name                  = "private-2c-2"
   common_prefix         = local.common_prefix
   common_tags           = local.common_tags
 }
@@ -54,9 +97,13 @@ resource "aws_route" "public_internet_access" {
   gateway_id             = module.igw.igw_id
 }
 
-# 퍼블릭 서브넷 연결
-resource "aws_route_table_association" "public" {
-  subnet_id      = module.subnet_public.id
+# 퍼블릭 서브넷 2개 연결
+resource "aws_route_table_association" "public_1a" {
+  subnet_id      = module.subnet_public_1a.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "public_2c" {
+  subnet_id      = module.subnet_public_2c.id
   route_table_id = aws_route_table.public.id
 }
 
@@ -73,8 +120,20 @@ resource "aws_route" "private_natgw_access" {
   nat_gateway_id         = module.natgw.natgw_id
 }
 
-# 프라이빗 서브넷 연결
-resource "aws_route_table_association" "private" {
-  subnet_id      = module.subnet_private.id
+# 프라이빗 서브넷 4개 연결
+resource "aws_route_table_association" "private_1a_1" {
+  subnet_id      = module.subnet_private_1a_1.id
+  route_table_id = aws_route_table.private.id
+}
+resource "aws_route_table_association" "private_1a_2" {
+  subnet_id      = module.subnet_private_1a_2.id
+  route_table_id = aws_route_table.private.id
+}
+resource "aws_route_table_association" "private_2c_1" {
+  subnet_id      = module.subnet_private_2c_1.id
+  route_table_id = aws_route_table.private.id
+}
+resource "aws_route_table_association" "private_2c_2" {
+  subnet_id      = module.subnet_private_2c_2.id
   route_table_id = aws_route_table.private.id
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,6 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "subnet_private_id" {
-  value = module.subnet_private.id
-}
-
 output "public_route_table_id" {
   value = aws_route_table.public.id
 }
@@ -14,18 +10,48 @@ output "private_route_table_id" {
   value = aws_route_table.private.id
 }
 
-output "public_route_table_association_id" {
-  value = aws_route_table_association.public.id
-}
-
-output "private_route_table_association_id" {
-  value = aws_route_table_association.private.id
-}
-
 output "public_route_id" {
   value = aws_route.public_internet_access.id
 }
 
 output "private_route_id" {
   value = aws_route.private_natgw_access.id
+}
+
+output "subnet_public_1a_id" {
+  value = module.subnet_public_1a.id
+}
+output "subnet_public_2c_id" {
+  value = module.subnet_public_2c.id
+}
+output "subnet_private_1a_1_id" {
+  value = module.subnet_private_1a_1.id
+}
+output "subnet_private_1a_2_id" {
+  value = module.subnet_private_1a_2.id
+}
+output "subnet_private_2c_1_id" {
+  value = module.subnet_private_2c_1.id
+}
+output "subnet_private_2c_2_id" {
+  value = module.subnet_private_2c_2.id
+}
+
+output "public_route_table_association_1a_id" {
+  value = aws_route_table_association.public_1a.id
+}
+output "public_route_table_association_2c_id" {
+  value = aws_route_table_association.public_2c.id
+}
+output "private_route_table_association_1a_1_id" {
+  value = aws_route_table_association.private_1a_1.id
+}
+output "private_route_table_association_1a_2_id" {
+  value = aws_route_table_association.private_1a_2.id
+}
+output "private_route_table_association_2c_1_id" {
+  value = aws_route_table_association.private_2c_1.id
+}
+output "private_route_table_association_2c_2_id" {
+  value = aws_route_table_association.private_2c_2.id
 }
