24ca3e1607e648b8bf764e181c87cd05f2f966f3
yeeun <yeeun@yeeunui-MacBookPro.local>
2025-07-23 15:30:31 +0900

    fix : 필요없는 주석 삭제

--- terraform/modules/network/clientvpn/main.tf
+++ terraform/modules/network/clientvpn/main.tf
@@ -1,11 +1,11 @@
-# resource "aws_ec2_client_vpn_endpoint" "this" {
-#   description            = var.description
-#   server_certificate_arn = var.server_certificate_arn
-#   authentication_options {
-#     type                       = "certificate-authentication"
-#     root_certificate_chain_arn = var.root_certificate_chain_arn
-#   }
-#   client_cidr_block      = var.client_cidr_block
-#   vpc_id                 = var.vpc_id
-#   tags                   = merge(var.common_tags, { Name = "${var.common_prefix}clientvpn" })
-# } 
+resource "aws_ec2_client_vpn_endpoint" "this" {
+  description            = var.description
+  server_certificate_arn = var.server_certificate_arn
+  authentication_options {
+    type                       = "certificate-authentication"
+    root_certificate_chain_arn = var.root_certificate_chain_arn
+  }
+  client_cidr_block      = var.client_cidr_block
+  vpc_id                 = var.vpc_id
+  tags                   = merge(var.common_tags, { Name = "${var.common_prefix}clientvpn" })
+} 
