450c67720774afd140836165d71d3644ee1217af
1dyn <gksehddus202@naver.com>
2025-07-24 15:47:48 +0900

    fix: 노드 수 변경

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -33,9 +33,9 @@ resource "aws_eks_node_group" "this" {
   subnet_ids      = var.subnet_ids
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
+    desired_size = 3
+    max_size     = 5
+    min_size     = 3
   }
 
   tags = var.common_tags
