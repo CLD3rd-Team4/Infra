f713c71b8d64a508665c9b103ea99a7d750e4b68
조성욱 <>
2025-07-28 15:05:41 +0900

    Trigger terraform plan with updated variables

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -71,4 +71,4 @@ provider "kubectl" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
-}
+} 
