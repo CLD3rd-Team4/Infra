9a88c6fffe6134ad10189f01ea8b4f308cee548c
siiiirru <eqeee@naver.com>
2025-08-07 10:04:46 +0900

    fix:웹사이트 cdn 설정변경

--- terraform/main.tf
+++ terraform/main.tf
@@ -147,7 +147,7 @@ module "s3_website_bucket" {
 
   # --- S3 버킷 설정 ---
   bucket_name = "website"
-  is_public   = true
+  is_public   = false
   cloudfront_oai_arn = module.cloudfront.oai_iam_arn
 }
 
